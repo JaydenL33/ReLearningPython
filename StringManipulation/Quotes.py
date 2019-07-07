96572e8e4adc0d9c1c45acca25ddf0999f9028d7
rlcaust <Jayden@rlc.com.au>
2019-07-07 16:59:44 +1000

    Test Things

---
+++ StringManipulation/Quotes.py
@@ -0,0 +1,16 @@
+###############################################################################
+# Author: Jayden Lee
+# Date: 29/06/19
+# Purpose: This is a simple script for testing string manipluation.  
+
+###############################################################################
+
+print("This is a test.\n Hello, with a double quote") # Double-Quote
+print('This is a test.\n Hello, with a single quote.') # Single-Quote
+print(r'This is a test.\n Hello, with a r for raw.') # with an r. 
+print('''Wow. 
+Look at this. 
+This is using no escape chars. 
+
+This is cool. 
+''') # Triple-Quote
