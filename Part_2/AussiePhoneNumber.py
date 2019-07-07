307e16728e97df17f1918992db73def3c19ed5b7
rlcaust <Jayden@rlc.com.au>
2019-07-07 20:14:41 +1000

    Update AussiePhoneNumber.py
    Because Whitespace is removed, it won't actually work. because of the spam of other numbers. As it hits size 12.

--- Part_2/AussiePhoneNumber.py
+++ Part_2/AussiePhoneNumber.py
@@ -10,37 +10,41 @@
 
 #############################################################################
 # Input: phoneNumber String
-# This function checks if the phoneNumber is an Australian Phone Numbr
+# This function checks if the phoneNumber is an Australian Phone Number
 # Output: True or False
 #############################################################################
 
-#0412 235 235
 
 def isAussieNumber(phoneNumber):
 	RemovedphoneNumber = phoneNumber.replace(' ', '')
 	if len(RemovedphoneNumber) != 10:
-		print("Length needs to be 12.")
+		#print("Length needs to be 12.")
 		return False
 	for i in range(0,4):
 		# print(phoneNumber[i]) #DEBUG
 		if RemovedphoneNumber[i].isalpha():
-			print("Phonenumber contains letters. ")
+			#print("Phonenumber contains letters. ")
 			return False
 	for i in range(5,7):
 		if RemovedphoneNumber[i].isalpha():
-			print("Phone number contains letters. ")
+			#print("Phone number contains letters. ")
 			return False
 	for i in range(7,10):
 		if RemovedphoneNumber[i].isalpha():
-			print("Phone number contains letters. ")
+			#print("Phone number contains letters. ")
 			return False
 	return True
 
+def insideMessage(text):
+	for i in range(len(text)):
+		chunk = text[i:i+12]
+		if isAussieNumber(chunk) == True:
+			print(chunk, "is an Australian Phone Number.")
 
 #############################################################################
 # 								Main Code. 
 #############################################################################
-
+"""
 print("The number should follow the following format: 04xx xxx xxx")
 
 phoneNumber = input("Please input a Australian Phone Number: ")
@@ -49,3 +53,13 @@ while phoneNumber != "":
 	if isAussieNumber(phoneNumber):
 		print(phoneNumber, "is valid.")
 	phoneNumber = input("Please input a Australian Phone Number: ")
+"""
+
+message = """0458 770 196 is my phone number. Call me. or don't. 
+awdojawijdjiawdjiawdjiaw awopd jawipdj waipdjwaip djwpdijw ijdw j
+dhawdwadhawdoiahdoawhd 8wadh awodh awoudh waoudh waoudh 
+awdhawd98hawd8 hawd8 ahwd8awh d8wad
+02309 39492384892 39324 0412 234 232 2039239819382389231
+bad number man."""
+
+insideMessage(message)
