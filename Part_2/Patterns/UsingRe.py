8422808d18d2aab85a1d98c79d907d3c763d7b8f
rlcaust <Jayden@rlc.com.au>
2019-07-09 19:58:32 +1000

    Created UsingRe.py
    Worked but my class didn't work lol . Also resuffled file structure.

--- Part_2/Patterns/UsingRe.py
+++ Part_2/Patterns/UsingRe.py
@@ -1,12 +1,12 @@
 #############################################################################
 # Author: Jayden Lee (Jayden.Lee@student.uts.edu.au)
-# Date: DATE
-# Purpose: FILL ME ...  PLEASE! 
+# Date: 09/07/19
+# Purpose: To Understand How to use Regex and hopefully classes. 
 #############################################################################
 
 #############################################################################
 
-import somecode
+import re
 
 #############################################################################
 
@@ -15,14 +15,22 @@ import somecode
 #############################################################################
 
 #############################################################################
-# Input: input
-# This function DOES
-# Output: input
+# Input: null
+# This function changes the re compile code to find patterns. 
+# Output: null
 #############################################################################
 
-def aFunFuction(input):
-	return input
+class RegexQuery: 
+    def __init__(self): 
+        self.mobilePhoneNum = re.compile(r'\d\d\d\d \d\d\d \d\d\d')
+        self.homePhoneNum = re.compile(r'\d\d\d\d \d\d\d\d')
 
 #############################################################################
 # 								Main Code. 
 #############################################################################
+
+phoneNumRegex = re.compile(r'\d\d\d\d \d\d\d \d\d\d')
+
+query = phoneNumRegex.search("My Number is 0458 770 196")
+
+print("Found number is " + query.group())
