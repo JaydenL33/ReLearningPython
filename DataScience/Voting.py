286e2b0afc5409aa49d9e0222cba1825d08e6474
rlcaust <Jayden@rlc.com.au>
2019-10-01 16:06:47 +1000

    Added Changes

--- DataScience/Voting.py
+++ DataScience/Voting.py
@@ -3,7 +3,8 @@
 # Date: 1/10/2019
 # Purpose: In order to vote on the predicited values, I've used a vote. 
 # Meaning that if the the values occur more then 3 times, then it will be the
-# final voted value.
+# final voted value. For 31250 Introduction into Data Analytics.
+
 #############################################################################
 
 #############################################################################
@@ -15,44 +16,39 @@ import csv
 #############################################################################
 # 							Function and Class Def.
 #############################################################################
-class otherClass:
-    def __init__(self):
-    	self.x = 10
-    	self.str = "HELLO!"
-
-    def getData(self):
-        print("{0}+{1}j".format(self.x,self.x))
-
 
 #############################################################################
 # Input: Tables(The List of Tables), 
 # n (Being the of tables gone through already)
 # This function DOES
-# Output: input
+# Output: NULL
 #############################################################################
 
-def CSVLoad(tables, n):
-	with open(name + ".csv", 'r') as Voting:
+def CSVLoad(tables, name, n):
+	with open(name[n] + ".csv", 'r') as Voting:
 		read_data = csv.reader(Voting)
 
 	for i in range(read_data):
 		tables[i[n]] = int(read_data[i+1[2]])
-	return tables
 
 #############################################################################
-# Input: input
-# This function DOES
-# Output: input
+# Input: Tables(The extracted value as whether or not it was 1 or 0 for each
+# outputted result from Knime.
+# finalTable (The final table to write too.)
+# This function returns a list of weather a majority of the results were 1 
+# or 0.
+# Output: NULL
 #############################################################################
 
-def Voting(tables, finalTable):
+def Voting(tables, finalTable, Outputs):
 
 	for i in range(tables):
 		for l in range(tables[i]):
 			entrySum += tables[i[l]]
-		if entrySum >= 3:
+		if entrySum >= NumOfOutput/2:
 			finalTable[i] = 1
-	return finalTable
+		else: 
+			finalTable[i] = 0
 
 
 
@@ -63,3 +59,12 @@ def Voting(tables, finalTable):
 
 finalTable = []
 tables = []
+names = []
+
+NumOfOutput = 5
+
+for i in range(NumOfOutput):
+	name = input("What is the name of the CSV?")
+	CSVLoad(tables, name, i)
+
+Voting(tables, finalTable, NumOfOutput)
