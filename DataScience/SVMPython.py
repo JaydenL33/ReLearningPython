5f6d28399e3cbff05317f15c401bd1a699b8f5eb
rlcaust <Jayden@rlc.com.au>
2019-10-09 18:24:39 +1100

    Removed Outputs

--- DataScience/SVMPython.py
+++ DataScience/SVMPython.py
@@ -21,13 +21,13 @@ import time
 # Output: input
 #############################################################################
 
-def SVMRegression(X, y):
+def SVMRegression(X, y, output):
 
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=109)
 	#print(X_train)
 	#print(y_train)
 	start_time = time.time()
-	svclassifier = svm.SVC(kernel='rbf', degree=4, gamma='auto', max_iter = 1000, tol=0.001)
+	svclassifier = svm.SVC(kernel='rbf', degree=4, gamma='auto_deprecated', max_iter = 10, tol=0.001)
 
 	print("Fitting the data.")
 	print("##################################################")
@@ -41,6 +41,7 @@ def SVMRegression(X, y):
 	print("--- %s seconds to complete ---" % (time.time() - start_time))
 	#y_pred.to_csv(y_pred,"Prediction" sep=',', low_memory=False)
 
+	file = open(output+"_score.txt", 'w')
 
 	print("##################################################")
 	print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
@@ -48,12 +49,19 @@ def SVMRegression(X, y):
 	print("Recall:",metrics.recall_score(y_test, y_pred))
 	print("F Score:",metrics.f1_score(y_test, y_pred))
 
+	file.write("##################################################")
+	file.write("\nAccuracy:" + str(metrics.accuracy_score(y_test, y_pred)))
+	file.write("\nPrecision:" + str(metrics.precision_score(y_test, y_pred)))
+	file.write("\nRecall:" + str(metrics.recall_score(y_test, y_pred)))
+	file.write("\nF Score:" + str(metrics.f1_score(y_test, y_pred)))
 
-	dmp = pickle.dump(svclassifier, open('Outputs/NormalisedPolyPrediction.sav','wb'))
-	np.savetxt("NormalisedPolyPrediction.csv", y_pred, delimiter=",")
+	file.close()
+
+	dmp = pickle.dump(svclassifier, open(output + '.sav','wb'))
+	np.savetxt(output + ".csv", y_pred, delimiter=",")
 
 def pandasLoad(filename):
-	data = pd.read_csv("Inputs/"+filename + '.csv', sep=',', low_memory=False)
+	data = pd.read_csv("Inputs/"+ filename + '.csv', sep=',', low_memory=False)
 	X = data.drop(["QuoteConversion_Flag", "Original_Quote_Date"],axis=1)
 	y = data['QuoteConversion_Flag']
 	return X, y
@@ -66,4 +74,4 @@ filename = "SMOTEdProcessed"
 
 X, y = pandasLoad(filename)
 
-SVMRegression(X, y)
+SVMRegression(X, y, "Outputs/rbfPrediction")
